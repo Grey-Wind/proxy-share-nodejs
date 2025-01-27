fb52b6b2a72adddfb7d10b4e8771ba470c1cb53d
Grey Wind <wjunqin7@gmail.com>
2025-01-27 16:42:40 +0800

    feat: console

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const ProxyServer = require('./src/proxy.js');
-const path = require('path');
+const fs = require('fs');
 
+// SSL 配置（如果需要 HTTPS）
+const sslOptions = {
+    key: fs.readFileSync('openssl/private.key', 'utf8'),
+    cert: fs.readFileSync('openssl/certificate.crt', 'utf8'),
+};
+
+// 创建代理服务器实例
+const proxyServer = new ProxyServer('localhost', 7890, sslOptions);
+
+// 启动代理服务器
+proxyServer.start(1234);
